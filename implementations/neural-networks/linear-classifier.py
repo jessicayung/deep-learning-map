8c0a8bcb74485264ed7a38a799bfe35db4ae1900
Jessica Yung <jessicayungyc@gmail.com>
2018-01-04 15:52:14 +0800

    feat(impl): train linear softmax classifier - code from cs231n

--- implementations/neural-networks/linear-classifier.py
+++ implementations/neural-networks/linear-classifier.py
@@ -1,3 +1,8 @@
+"""
+Adapted from CS231n http://cs231n.github.io/neural-networks-case-study/
+
+Jan 2018
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,11 +16,17 @@ N = 100
 D = 2
 # Number of classes
 K = 3
+num_examples = N*K
+
+# Parameters
+# regularisation strength
+reg = 1e-3
+step_size = 1e-0
 
 # Initialise data matrix
-X = np.zeros((N*K, D))
+X = np.zeros((num_examples, D))
 # Initialise vector of labels
-y = np.zeros(N*K, dtype='uint8')
+y = np.zeros(num_examples, dtype='uint8')
 # Populate data matrix
 for j in range(K):
     ix = range(N*j, N*(j+1))
@@ -27,4 +38,44 @@ for j in range(K):
     y[ix] = j
 
 plt.scatter(X[:, 0], X[:, 1], c=y, s=40)
-plt.show()
+# plt.show()
+
+# Softmax classifier
+
+# Initialise parameters
+W = 0.01 * np.random.randn(D, K)
+b = np.zeros((1, K))
+
+n_epochs = 10
+for i in range(n_epochs):
+    # Forward pass
+    scores = np.dot(X, W) + b
+    #print(scores.shape)
+    #print(scores[:5])
+    exp_scores = np.exp(scores)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # sum along each row
+    correct_logprobs = -np.log(probs[range(num_examples), y])
+
+    data_loss = np.sum(correct_logprobs)/num_examples
+    reg_loss = 0.5*reg*np.sum(W*W)
+    loss = data_loss + reg_loss
+    print("Epoch: %d, Loss: %f" % (i, loss))
+    # print("Initial loss should be about np.log(1.0/3) = ", np.log(1.0/3))
+
+    # Backprop
+    # dLi/dfk = pk - 1(yi=k)
+    dscores = probs
+    dscores[range(num_examples),y] -= 1
+    dscores /= num_examples
+    db = np.sum(dscores, axis=0, keepdims=True)  # sum across columns
+    dW = np.dot(X.T, dscores)
+    dW += reg*W
+
+    # Parameter update
+    W += -step_size * dW
+    b += step_size * db
+
+# Evaluate training set accuracy
+scores = np.dot(X, W) + b
+predicted_class = np.argmax(scores, axis=1)
+print("Training accuracy: %.2f" % (np.mean(predicted_class == y)))
